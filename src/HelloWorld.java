490014f904cd9495df7051f6bd9f32887801595f
Bernat <bernat.orellana@gmail.com>
2020-03-02 15:29:51 +0100

    canvi bernat

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,9 @@ public class GoodBye!! {
     // Canvi al repo2
     // Canvi original(repo1)
 
+    public int suma(int a , int b){
+        return a+b;
+    }
 
     public static satanicMethod666(int i)
     {
